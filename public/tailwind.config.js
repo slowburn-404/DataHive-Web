a63028215b6f5c70b1c9b97a1efbe0178eb6136e
slowburn-404 <borisochola32@gmail.com>
2023-06-06 20:27:54 +0300

    initial commit

---
+++ public/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                Inter: ['Inter', 'sans-serif']
+            },
+            colors: {
+                'bee-yellow': '#e8a917',
+                'black': '#000000'
+
+            }
+        },
+    },
+    plugins: [],
+}
